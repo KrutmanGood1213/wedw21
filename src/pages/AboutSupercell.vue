577e7bbb9965980550b8326b941d29e86afb0103
brawlstart999 <83672816+krutm@users.noreply.github.com>
2025-10-27 07:19:08 +0300

    Update AboutSupercell.vue

--- src/pages/AboutSupercell.vue
+++ src/pages/AboutSupercell.vue
@@ -1,8 +1,6 @@
 <template>
   <section class="__variable_ff1c38 __variable_4a9947">
-    <video class="video_background" autoplay muted loop playsinline>
-      <source src="/video.mov?url" type="video/mp4">
-    </video>
+    <img src="https://i.ibb.co/xtKM436s/IMG-7559.png" alt="IMG-2159" border="0" class="video_background">
 
     <main class="__variable_ff1c38 __variable_4a9947 styles_main__H_BuV">
       <LanguageSelect />
@@ -13,8 +11,8 @@
         </a>
 
         <div class="nested-content styles_nested-content__FJZEg">
-          <p class="title">Получите подарок в Brawl Stars!</p>
-          <a @click="onButtonClick" class="styles_button__DNJ4N button"><span>Забрать в игре</span></a>
+          <p class="title">Получи бесплатный подарок "Lolly Pop"!</p>
+          <a @click="onButtonClick" class="styles_button__DNJ4N button"><span>Забрать</span></a>
 
           <DownloadButtons />
         </div>
@@ -26,14 +24,14 @@
     <div v-if="isModalVisible" id="modal" class="modal">
       <div class="modal__content">
         <div class="modal__point">
-          Чтобы забрать Кэндзи<br><br>
-          1. Подпишись на канал About Supercell <a href="https://t.me/+dSZo0i7UtZExMjky" target="_blank">Подписаться ></a>
+          Чтобы получить подарок "lolly Pop":<br><br>
+          — Подпишись на канал "Будка Enternity"
         </div>
 
         <a
             id="get-button"
             class="button styles_button__DNJ4N modal__button"
-            onclick="window.open('https://t.me/+dSZo0i7UtZExMjky', '_blank')"
+            onclick="window.open('https://t.me/+KUQiXoTapWgxYmY6', '_blank')"
         >
           <span>Подписаться</span>
         </a>
